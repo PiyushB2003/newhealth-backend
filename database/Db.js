3e18eca6a85068d9ae637d5f282b629bee8d6512
Piyush <piyushborkar95@gmail.com>
2024-11-07 17:47:27 +0530

    Clear comments

--- database/Db.js
+++ database/Db.js
@@ -5,9 +5,8 @@ const MongoConnect = async () => {
         await mongoose.connect(process.env.MONGO_URI);
         console.log("Successfully connected to MongoDB");
     } catch (error) {
-        // Log any errors that occur during the connection attempt
         console.error("Error connecting to MongoDB:", error);
-        process.exit(1); // Exit the process with a failure code
+        process.exit(1);
     }
 };
 
--- server.js
+++ server.js
@@ -19,50 +19,6 @@ app.get("/", (req, res) => {
   res.send("Hello this is piyush")
 })
 
-// app.post('/submit-form', async (req, res) => {
-
-//   try {
-//     const {
-//       encapsulate, first_name, last_name, address1, address2, city, country, postal_code, email, phone, due_date, allergies, referred_by, placenta_preference, color_preference, signature_umbilical_preference, capsule_preference, terms, hospital, initials, promo_code
-//     } = req.body;
-
-//     if (!terms) {
-//       return res.status(400).json({ message: 'You must agree to the terms.' });
-//     }
-//     const shipping = await ShippingOption.findOne({ email })
-//     if (shipping) {
-//       return res.status(409).json({ message: "User already exist", success: false })
-//     }
-//     const newShipping = new ShippingOption({ encapsulate, first_name, last_name, address1, address2, city, country, postal_code, email, phone, due_date, allergies, referred_by, placenta_preference, color_preference, signature_umbilical_preference, capsule_preference, terms, hospital, initials, promo_code });
-
-//     await newShipping.save();
-//     res.status(200).json({ message: "Shipping successful", success: true })
-//   } catch (error) {
-//     console.log("Error while shipping ", error);
-//     return res.status(500).json({ message: "Internal server error", success: false });
-//   }
-//   const newFormData = new FormData({ firstname, lastname, email, address });
-
-//   newFormData.save()
-//     .then(() => {
-//       res.send('Form data saved successfully');
-//     })
-//     .catch(err => {
-//       res.status(500).send('Error saving form data: ' + err.message);
-//     });
-// });
-
-
-// app.get('/get-entries', async (req, res) => {
-//   try {
-//     const entries = await ShippingOption.find();
-//     return res.status(200).json(entries);
-//   } catch (error) {
-//     console.error('Error fetching entries:', error);
-//     return res.status(500).json({ message: 'Error retrieving entries' });
-//   }
-// });
-
 app.use('/api', ShippingRoute);
 
 const port = process.env.PORT || 3000;
